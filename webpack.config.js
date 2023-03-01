fa5d3c9226c01f1ec4332cc5206ea76eed5b4a1a
OLGON92 <ogon318@gmail.com>
2023-02-28 19:18:42 -0800

    fix route
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>
    Co-authored-by: Quin Asselin <quin.asselin1@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/power-plant.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -19,7 +19,7 @@ module.exports = {
       verbose: true
     }),
     new HtmlWebpackPlugin({
-      title: 'Galactic Calculator',
+      title: 'Power-Plant',
       template: './src/index.html',
       inject: 'body'
     })
