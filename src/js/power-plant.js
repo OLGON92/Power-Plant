b14b229fae15a0125480a78a736c1aac429c6990
OLGON92 <ogon318@gmail.com>
2023-02-28 19:20:17 -0800

    add UI logic
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>
    Co-authored-by: Quin Asselin <quin.asselin1@gmail.com>

--- src/js/power-plant.js
+++ src/js/power-plant.js
@@ -27,34 +27,47 @@
 //   }
 // }
 
-const changeState = (prop) => {
-  return (value) => {
-    return (state) => ({
-      ...state,
-      [prop] : (state[prop] || 0 ) + value
-    })
-  }
-}
-
-const feed = changeState("soil");
-const hydrate = changeState("water");
-const giveLight = changeState("light");
-
-const bigFood = feed(5);
-const normalFood = feed(1);
-const waterPlus = hydrate(10);
-const waterRegular = hydrate(1);
-const doubleSun = giveLight(2);
-const singleSun = giveLight(1);
-
-
 const storeState = () => {
   let currentState = {};
   return (stateChangeFunction = state => state) => {
     const newState = stateChangeFunction(currentState);
     currentState = {...newState};
     return newState;
-  }
-}
+  };
+};
 
 const stateControl = storeState();
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0 ) + value
+    });
+  };
+};
+
+// const feed = changeState("soil");
+// const hydrate = changeState("water");
+// const giveLight = changeState("light");
+
+const bigFood = changeState("feed")(5);
+/* const normalFood = changeState("feed")(1);
+const waterPlus = hydrate(10);
+const waterRegular = hydrate(1);
+const doubleSun = giveLight(2);
+const singleSun = giveLight(1);
+ */
+
+window.onload = function() { 
+
+  document.getElementById('feed').onclick = function() {
+    const newState = stateControl(bigFood);
+    document.getElementById('soil-value').innerText = `Soil: ${newState.feed}`;
+  };
+
+  document.getElementById('show-state').onclick = function() {
+    const currentState = stateControl();
+    document.getElementById('soil-value').innerText = `Soil: ${currentState.feed}`;
+  };
+};
